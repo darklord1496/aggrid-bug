d3d46df08adf4603da8dcd26b0fb13d659809731
Jakub Bejvl <jbejvl@ra.rockwell.com>
2022-08-29 11:21:26 +0200

    Ag grid bug with scrolling - initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {AgGridAngular} from 'ag-grid-angular';
+import {GridOptions} from 'ag-grid-community';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'aggridbug';
+
+  @ViewChild('agGrid', { static: false }) agGrid: AgGridAngular;
+
+  gridOptions: GridOptions = {};
+
+  columnDefs = [
+    {
+      field: 'id',
+    },
+    {
+      field: 'name',
+      wrapText: true,
+      width: 150,
+      autoHeight: true,
+    },
+    {
+      field: 'price',
+    },
+  ];
+
+  rowData = this.fillData(50);
+
+  tableWidth = 500;
+
+  tableHeight = 600;
+
+  rowHeight = 60;
+
+  onGridReady(params: any) {}
+
+  fillData(rows: number): any[] {
+    const data = [];
+    for (let i = 0; i < rows; i++) {
+      data.push({
+        id: i,
+        name: 'Long long long long long long long long long name' + i,
+        price: i * 1000,
+      });
+    }
+    return data;
+  }
 }
